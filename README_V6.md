ae3d48f0919f430cb92a3e2054fa1271b949d6fe
vp2pj4fbz7-lang <vp2pj4fbz7@privaterelay.appleid.com>
2026-08-28 14:13:29 +0200

    Add files via upload

---
+++ README_V6.md
@@ -0,0 +1,19 @@
+Elektronik Diag V6 – Akademia Diagnostyki
+
+Nowości V6:
+- osobna zakładka Akademia Diagnostyki,
+- 20 lekcji szkoleniowych z diagnostyki elektroniki,
+- quiz na końcu każdej lekcji,
+- zapisywanie ukończonych lekcji w localStorage,
+- pasek postępu,
+- interaktywny Trener usterek z przypadkami warsztatowymi,
+- zachowane moduły V5: Diagnostyka PRO, Internet, Atlas podzespołów i Datasheet,
+- cache Service Workera zmieniony na elektronik-diag-v6-0.
+
+Instalacja GitHub Pages:
+1. Wgraj index.html, sw.js i manifest.webmanifest.
+2. Zachowaj/wgraj swoje istniejące icon-192.png i icon-512.png, jeśli były używane w repozytorium.
+3. Po aktualizacji odśwież stronę. Jeśli urządzenie długo pokazuje starą wersję, zamknij PWA/kartę i otwórz ponownie.
+
+Uwaga bezpieczeństwa:
+Materiały są pomocą szkoleniową. Pomiary części sieciowej, HV i akumulatorów Li-ion wymagają odpowiednich kwalifikacji, izolacji i sprzętu pomiarowego.
--- sw.js
+++ sw.js
@@ -1,15 +1,5 @@
-const CACHE='elektronik-diag-v5-2';
+const CACHE='elektronik-diag-v6-0';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{
-  if(e.request.mode==='navigate'){
-    e.respondWith(fetch(e.request).then(r=>{
-      const copy=r.clone();
-      caches.open(CACHE).then(c=>c.put('./index.html',copy));
-      return r;
-    }).catch(()=>caches.match('./index.html')));
-  }else{
-    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-  }
-});
+self.addEventListener('fetch',e=>{if(e.request.mode==='navigate')e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return r;}).catch(()=>caches.match('./index.html')));else e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
