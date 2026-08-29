23c5450a137c49b3c0f39304b6e150497a7b61ef
vp2pj4fbz7-lang <vp2pj4fbz7@privaterelay.appleid.com>
2026-08-29 16:56:05 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,1 @@
-const CACHE='elektronik-diag-v6-7-3';
-self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('activate',e=>e.waitUntil(
- caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
- .then(()=>self.clients.claim())
-));
-self.addEventListener('fetch',e=>{
- if(e.request.mode==='navigate'){
-  e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match('./index.html')));
- }
-});
+const CACHE='elektronik-diag-v6-7-4';self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>{if(e.request.mode==='navigate')e.respondWith(fetch(e.request,{cache:'no-store'}));});
